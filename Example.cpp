0ccdb462c0e49bebde7e62541dab908df6f8629a
zogov123 <danielkrich147@gmail.com>
2022-01-24 17:24:53 +0200

    Update

--- Example.cpp
+++ Example.cpp
@@ -3,40 +3,40 @@
 
 int main()
 {
-	dk::TCPServer* server = new dk::TCPServer(2227);
-	return 0;
+    dk::TCPServer* server = new dk::TCPServer(2227);
+    return 0;
 }
 
 void dk::TCPServer::OnServerStartup(dk::TCPServer* server)
 {
-	SetConsoleTitleA(std::string("Running on port: " + std::to_string(server->port)).c_str());
-	std::cout << "Server is running..." << std::endl;
+    SetConsoleTitleA(std::string("Running on port: " + std::to_string(server->port)).c_str());
+    std::cout << "Server is running..." << std::endl;
 }
 
 bool dk::TCPServer::OnClientConnect(dk::ConnectedClient* client)
 {
-	if (client->serverInstance->connectedClients.size() < 3)
-	{
-		std::cout << "client connected, IP: " << client->GetIP() << std::endl;
-		client->Send("[SERVER] Welcome, " + client->GetIP());
-		return true;
-	}
-	else
-	{
-		std::cout << client->GetIP() << " tried to connect, but server reached max online" << std::endl;
-		client->Send("[SERVER] Reached maximum online.");
-		return false;
-	}
+    if (client->serverInstance->connectedClients.size() < 3)
+    {
+        std::cout << "client connected, IP: " << client->GetIP() << std::endl;
+        client->Send("[SERVER] Welcome, " + client->GetIP());
+        return true;
+    }
+    else
+    {
+        std::cout << client->GetIP() << " tried to connect, but server reached max online" << std::endl;
+        client->Send("[SERVER] Reached maximum online.");
+        return false;
+    }
 }
 
 void dk::TCPServer::OnMessageReceive(dk::ConnectedClient* client, std::vector<char>* data)
 {
-	std::cout << "IP: " << client->GetIP() << ", Data: " << data->data() << std::endl;
-	client->Send("[SERVER] You've sent: " + std::string(data->data()));
+    std::cout << "IP: " << client->GetIP() << ", Data: " << data->data() << std::endl;
+    client->Send("[SERVER] You've sent: " + std::string(data->data()));
 }
 
 void dk::TCPServer::OnClientDisconnect(dk::ConnectedClient* client)
 {
-	std::cout << "IP: " << client->GetIP() << ", disconnected" << std::endl;
+    std::cout << "IP: " << client->GetIP() << ", disconnected" << std::endl;
 }
 
